dfcf72ce008cd3c17039bd2e2ad579c43e606e2d
Bismark668 <147917491+Bismark668@users.noreply.github.com>
2024-02-28 16:20:33 +0000

    I just added a forms page

---
+++ script.js
@@ -0,0 +1,34 @@
+const navBtn = document.querySelector('#POP-UP')
+const homeBtn =document.querySelector('#home-btn')
+const bookBtn =document.querySelector('#book-us')
+const closeBtn =document.querySelector('#close')
+
+
+// /Toggling the forms page with the Nvbar button/ 
+
+navBtn.addEventListener('click',function(){
+    let formsSection = document.querySelector('.sec-4')
+    formsSection.classList.add('active')
+})
+
+// /Toggling the forms page with the home button/ 
+
+homeBtn.addEventListener('click',function(){
+    let formsSection = document.querySelector('.sec-4')
+    formsSection.classList.add('active')
+})
+
+// /Using the book button to book and close the forms section/ 
+
+bookBtn.addEventListener('click',function(){
+    alert('THANKS FOR CHOSING US')
+    let formsSection = document.querySelector('.sec-4')
+    formsSection.classList.remove('active')
+})
+
+// /Using the colse button to close the forms section/ 
+
+closeBtn.addEventListener('click',function(){
+    let formsSection = document.querySelector('.sec-4')
+    formsSection.classList.remove('active')
+})
